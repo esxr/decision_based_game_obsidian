b40cc40ddb97fae3c36cbeca85568858a1336830
Pranav Dhoolia <dhoolia.pranav@gmail.com>
2022-04-26 17:51:31 +1000

    make basic game loop

--- public/game.js
+++ public/game.js
@@ -2,11 +2,55 @@ const textElement = document.getElementById('text');
 const optionButtonsElement = document.getElementById('option-buttons');
 
 // console.log(path.join(__dirname, 'sampleFile.md'))
+var state = {}
+
+async function getState() {
+    // delete all option buttons
+    while (optionButtonsElement.firstChild) {
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+    }
+
+    fetch('/state')
+        .then(response => response.json())
+
+        // populate prompt
+        .then(data => { textElement.innerText = data.prompt; return data })
+
+        // populate options
+        .then(data => {
+            // set option-buttons from the data
+            data.options.forEach(option => {
+
+                // create option button
+                const button = document.createElement('button');
+                button.classList.add('btn');
+                button.classList.add('btn-one');
+                button.innerText = option;
+
+                button.addEventListener('click', async () => {
+                    var res = await changeState(option)
+                    await getState()
+                });
+                optionButtonsElement.appendChild(button);
+            });
+        })
+}
+
+async function changeState(option) {
+    await fetch('/state', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ filePath: option })
+    }).then(res => {
+        if (res.status == 200) return true
+    })
+}
 
 function startGame() {
-    fetch('/state')
-    .then(response => response.json())
-    .then(data => { textElement.innerText = data.prompt })
+    getState()
 }
 
 
--- server.js
+++ server.js
@@ -4,12 +4,41 @@ var path = require('path');
 const fs = require('fs').promises;
 
 //app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+// state management
+var state = {
+
+}
+
+function game() {
+    state = getStateFromFile(path.join(__dirname, 'start.md'))
+}
+
+function changeState(option) {
+    state = getStateFromFile(option)
+}
+
+function getCurrentState() {
+    return state
+}
 
 app.get('/state',
     async function (req, res) {
-        res.send(await getStateFromFile(path.join(__dirname, 'sampleFile.md'))
-        )
+        res.send(await getCurrentState())
+    }
+)
+
+app.post('/state',
+    async function (req, res) {
+        if (req.body.filePath)
+            changeState(req.body.filePath)
+
+        res.sendStatus(200);
     }
 )
 
@@ -34,4 +63,5 @@ async function getStateFromFile(filePath) {
 }
 
 app.listen(80);
+game()
 console.log('Listening on port 80');
